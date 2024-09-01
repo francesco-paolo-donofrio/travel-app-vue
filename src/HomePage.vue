d1c56e5baccd26e0ca2e240a0cc7f8213909dc3e
Frankosviz <geom.francescopaolo@gmail.com>
2024-09-01 23:21:07 +0200

    HomePage component

---
+++ src/HomePage.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+import { store } from './store';
+    export default {
+        name: 'HomePage',
+        components: {
+        
+        },
+        data() {
+            return {
+                store,
+            }
+        },
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
